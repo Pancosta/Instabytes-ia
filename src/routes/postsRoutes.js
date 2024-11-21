bcd0f4082cc53bd143b1fc13fc8770e0b6453ea1
Pancosta <dz6.gustav@gmail.com>
2024-11-21 16:02:27 -0300

    adicionando upload de imagens com multer

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,17 @@
 import express from "express";
-import { enviarPost, listarPosts } from "../controllers/postsControler.js";
+import multer from "multer";
+import { enviarPost, listarPosts, uploadImagem} from "../controllers/postsControler.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ dest: "./uploads" , storage});
 
 const routes = (app) => {
     // Configura o middleware para interpretar requisições no formato JSON
@@ -7,6 +19,7 @@ const routes = (app) => {
     // Define uma rota GET para "/posts" que retorna todos os posts
     app.get("/posts", listarPosts);
     app.post("/posts", enviarPost);
+    app.post("/upload", upload.single("imagem"),uploadImagem);
 }
 
 export default routes;
